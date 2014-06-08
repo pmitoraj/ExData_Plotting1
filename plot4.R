8702af977fb63df7b43ea0188377a6d4d246592a
Piotr Mitoraj <piotr.mitoraj@gmail.com>
2014-06-09 00:17:43 +0300

    added R code and png file for plot 4

---
+++ plot4.R
@@ -0,0 +1,66 @@
+
+# define coloumn names
+colnames <- c('Date', 'Time', 'Global_active_power', 'Global_reactive_power',
+              'Voltage', 'Global_intensity','Sub_metering_1','Sub_metering_2',
+              'Sub_metering_3')
+
+# read data
+household <- read.table('../household_power_consumption.txt',header=TRUE,
+                        sep=";", na.strings="?",
+                        col.names=colnames)
+
+# convert date column to Date class
+household$Date <- as.Date(household$Date,format="%d/%m/%Y")
+
+# subset the data
+household_subset <- subset(household, Date >= as.Date("2007-02-01") &
+                           Date <= as.Date("2007-02-02"))
+time <- seq(as.POSIXct("2007-02-01 00:00:00"),by="min",length.out=nrow(household_subset))
+household_subset$Time <- time
+
+# save the plot into png file
+png(filename="figure/plot4.png")
+# create the grid
+par(mfrow=c(2,2))
+# 1 plot
+plot(household_subset$Time,
+     household_subset$Global_active_power,
+     type="l",
+     ylab="Global Active Power (Kilowatts)",
+     xlab="")
+# 2 plot
+plot(household_subset$Time,
+     household_subset$Voltage,
+     type="l",
+     ylab="Voltage",
+     xlab="datetime")
+
+# 3 plot
+plot(household_subset$Time,
+     household_subset$Sub_metering_1,
+     type="l",
+     ylab="Energy sub metering",
+     xlab="",
+     col='black')
+lines(household_subset$Time,
+     household_subset$Sub_metering_2,
+     type="l",
+     ylab="Energy sub metering",
+     xlab="",
+     col='red')
+lines(household_subset$Time,
+     household_subset$Sub_metering_3,
+     type="l",
+     ylab="Energy sub metering",
+     xlab="",
+     col='blue')
+legend(x='topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),
+       col=c("black","red","blue"),lty=c(1,1,1))
+# 4 plot
+plot(household_subset$Time,
+     household_subset$Global_reactive_power,
+     type="l",
+     ylab="Global_reactive_power",
+     xlab="datetime")
+dev.off()
+
